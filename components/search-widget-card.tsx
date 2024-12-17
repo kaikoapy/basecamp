40c4d084feebf7d0b244af9a98186b453348f39c
kaikoapy <156684867+kaikoapy@users.noreply.github.com>
2024-12-16 21:28:44 -0500

    sd

--- components/search-widget-card.tsx
+++ components/search-widget-card.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { CommandSearch } from "@/components/sidebar/command-search";
+import { CommandSearch } from "@/components/command-search";
 import { cn } from "@/lib/utils";
 
 export function SearchWidgetCard() {
