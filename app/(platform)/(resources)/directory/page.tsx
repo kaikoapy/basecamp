5ec78978a90689cf65ad599f297ba29a7a60ce6c
kaikoapy <156684867+kaikoapy@users.noreply.github.com>
2025-01-08 02:15:25 -0500

    ds

--- app/(platform)/(resources)/directory/page.tsx
+++ app/(platform)/(resources)/directory/page.tsx
@@ -250,7 +250,7 @@ const ContactDirectory = () => {
               <h3 className="text-lg font-medium text-gray-700 border-b pb-2">
                 {position}
               </h3>
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
                 {positionContacts.map((contact) => (
                   <ContactCard key={contact._id} contact={contact} />
                 ))}
