b3ffe60c011a7d91559a9355bc128856d443cf39
kaikoapy <156684867+kaikoapy@users.noreply.github.com>
2025-01-18 01:04:05 -0500

    fd

--- components/ui/shimmer-button.tsx
+++ components/ui/shimmer-button.tsx
@@ -11,7 +11,7 @@ interface ShimmerButtonProps {
 const ShimmerButton = ({ onClick, children }: ShimmerButtonProps) => {
   return (
     <motion.button
-      className="inline-flex overflow-hidden rounded-lg bg-blue-400 hover:bg-blue-500 transition-colors"
+      className="inline-flex overflow-hidden rounded-lg bg-zinc-800 hover:bg-zinc-900 transition-colors"
       whileTap={{
         scale: 0.95,
       }}
@@ -20,7 +20,7 @@ const ShimmerButton = ({ onClick, children }: ShimmerButtonProps) => {
       }}
       onClick={onClick}
     >
-      <span className="px-4 py-1.5 text-xs text-white flex items-center gap-2">
+      <span className="px-4 py-1.5 text-xs text-zinc-100 flex items-center gap-2">
         {children || (
           <>
             <Check className="h-4 w-4" />
