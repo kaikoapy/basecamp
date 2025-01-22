68295208e337e414661ffa3389e0bceba9577275
kaikoapy <156684867+kaikoapy@users.noreply.github.com>
2025-01-21 23:15:49 -0500

    d

--- app/(marketing)/[sign-in]/page.tsx
+++ app/(marketing)/[sign-in]/page.tsx
@@ -1,11 +1,21 @@
-import { SignIn, useUser } from '@clerk/nextjs'
+// app/(marketing)/[sign-in]/page.tsx
+
+"use client"; // This makes the component a client component
+
+import { useUser } from '@clerk/nextjs';
+import { SignIn } from '@clerk/nextjs';
 
 export default function Home() {
-  const { user } = useUser()
+  const { user } = useUser();
 
   if (!user) {
-    return <SignIn />
+    return <SignIn />;
   }
 
-  return <div>Welcome!</div>
+  return (
+    <div>
+      <h1>Welcome, {user.firstName}!</h1>
+      {/* Other content for signed-in users */}
+    </div>
+  );
 }
