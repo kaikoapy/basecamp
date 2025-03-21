7d978600cfbe87f7715bc0cc0d385e0909715206
kaikoapy <156684867+kaikoapy@users.noreply.github.com>
2025-03-21 02:41:41 -0400

    ds

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,18 +1,17 @@
 import Features from "./_components/FounderLetter"
 import ImageSection from "./_components/FeatureImages"
 import Hero from "./_components/Hero"
-import LogoCloud from "./_components/LogoCloud"
+import Testimonial from "./_components/Home-Testimonial"
 import { BentoDemo } from "./_components/bento-grid"
 
 export default function Home() {
   return (
     <main className="flex flex-col overflow-hidden">
       <Hero />
-      <LogoCloud />
+      <Testimonial />
       <ImageSection />
       <BentoDemo />
       <Features />
-  
     </main>
   )
 }
